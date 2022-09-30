df88c30af6df09f8a4caba7952198d1f4ce6d19f
Natã Carvalho <natancarvalho@poli.ufrj.br>
2022-09-30 15:54:55 -0300

    Add of the first classes. Events and Cars

---
+++ include/functions.h
@@ -0,0 +1,24 @@
+//
+// Created by natan on 21/09/22.
+//
+#ifndef DBCONNECT_FUNCTIONS_H
+#define DBCONNECT_FUNCTIONS_H
+
+using namespace std;
+
+#include <string>
+#include <vector>
+
+void cgiPage();
+
+void createDB();
+
+void pythonAPI();
+
+void connectDB();
+
+vector <string> read_csv_header(const char* pathCSV);
+
+string read_csv_data(const char* pathCSV);
+
+#endif //DBCONNECT_FUNCTIONS_H
---
+++ include/queries.h
@@ -0,0 +1,16 @@
+//
+// Created by natan on 27/09/22.
+//
+
+#ifndef KPIREPORT_QUERIES_H
+#define KPIREPORT_QUERIES_H
+
+#define DB_SERVER          "tcp://127.0.0.1:3306"
+#define DB_LOGIN           "root"
+#define DB_PASSWORD        "C@rvalhoBD"
+#define DB_SCHEMA          "KPIReport"
+
+
+
+
+#endif //KPIREPORT_QUERIES_H
